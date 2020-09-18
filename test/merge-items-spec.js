d2bfaee91a3a1ebcc8e32ba555494cf2ebf42b4a
Kyle Barthelmes <kbart2401@gmail.com>
2020-09-18 17:34:59 -0400

    Passing all merge-items tests

--- test/merge-items-spec.js
+++ test/merge-items-spec.js
@@ -9,18 +9,74 @@ describe("The mergeItems function", () => {
     </table>
   `;
   it("should return no <tr>s and no <td>s for no items", () => {
-    expect.fail('please write this test');
+    const items = [];
+    const result = mergeItems(template, items);
+    expect(result).does.include('<table>');
+    expect(result).does.include('</table>');
+    expect(result).does.include('<tbody>');
+    expect(result).does.include('</tbody>');
+
+    expect(result).does.not.include('<tr>');
+    expect(result).does.not.include('</tr>');
+    expect(result).does.not.include('<td>');
+    expect(result).does.not.include('</td>');
+
+    expect(result).does.not.include('<!--Content here-->')
+
   });
 
   it("should return a single <tr>, four <td>s, and a <form> for one uncompleted item", () => {
-    expect.fail('please write this test');
+    const item = [{title: 'Work it', category: 'Work'}]
+    const result = mergeItems(template, item);
+    expect(result).does.include('<table>');
+    expect(result).does.include('</table>');
+    expect(result).does.include('<tbody>');
+    expect(result).does.include('</tbody>');
+
+    expect(result).does.include('<tr>');
+    expect(result).does.include('</tr>');
+    expect(result).does.include('<td>Work it</td>');
+    expect(result).does.include('<td>Work</td>');
+    expect(result).does.include('<form method="POST" action="/items/1">');
+    
+    expect(result).does.not.include('<!--Content here-->')
   });
 
   it("should return a single <tr>, four <td>s, and no <form> for one completed item", () => {
-    expect.fail('please write this test');
+    const item = [{ title: 'Work it', category: 'Work', isComplete: true }]  
+    const result = mergeItems(template, item);
+    expect(result).does.include('<table>');
+    expect(result).does.include('</table>');
+    expect(result).does.include('<tbody>');
+    expect(result).does.include('</tbody>');
+
+    expect(result).does.include('<tr>');
+    expect(result).does.include('</tr>');
+    expect(result).does.include('<td>Work it</td>');
+    expect(result).does.include('<td>Work</td>');
+    expect(result).does.not.include('<form method="POST" action="/items/1">');
+
+    expect(result).does.not.include('<!--Content here-->')
   });
 
   it("should return three <tr>s for three items", () => {
-    expect.fail('please write this test');
+    const items = [{ title: 'Work it', category: 'Work', isComplete: true },
+      { title: 'Happy', category: 'Vacation', isComplete: false }, 
+      { title: 'Sad :(', category: 'Boring', isComplete: false }]
+      const result = mergeItems(template, items);
+    expect(result).does.include('<table>');
+    expect(result).does.include('</table>');
+    expect(result).does.include('<tbody>');
+    expect(result).does.include('</tbody>');
+
+    expect(result).does.include('<tr>');
+    expect(result).does.include('</tr>');
+    expect(result).does.include('<td>Work it</td>');
+    expect(result).does.include('<td>Work</td>');
+    expect(result).does.not.include('<form method="POST" action="/items/1">');
+    expect(result).does.include('<form method="POST" action="/items/2">');
+    expect(result).does.include('<form method="POST" action="/items/3">');
+
+    expect(result).does.not.include('<!--Content here-->')
   });
 });
