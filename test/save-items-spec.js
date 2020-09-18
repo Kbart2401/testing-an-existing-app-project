f8f7101e596cf4c3c1e08f29444507b11c49f9c6
Maximos Salzman <maximossalzman@gmail.com>
2020-09-18 18:28:48 -0400

    Finished writing tests

--- test/save-items-spec.js
+++ test/save-items-spec.js
@@ -2,10 +2,20 @@ const { expect } = require('chai');
 const { saveItems } = require('../save-items');
 describe("The saveItems function", () => {
   it('adds the new item to the list', () => {
-    expect.fail('please write this test');
+    const items = ["keys","wallet","phone"]
+    const newItem = "lint"
+
+    const result = saveItems(items, newItem)
+
+    expect(result).to.eql(["keys","wallet","phone","lint"]);
   });
 
   it('makes sure the result and the original are different', () => {
-    expect.fail('please write this test');
+    const items = ["keys", "wallet", "phone"]
+    const newItem = "lint"
+
+    const result = saveItems(items, newItem)
+
+    expect(result).does.not.eql(["keys", "wallet", "phone"])
   });
 });
